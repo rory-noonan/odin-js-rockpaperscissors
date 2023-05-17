7ae82cf35433d85b18121c66dea3f913e93f9112
rory-noonan <92176688+rory-noonan@users.noreply.github.com>
2023-05-17 14:08:40 -0600

    Add playRound function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,3 +4,26 @@ function getComputerChoice() {
   let index = Math.floor(Math.random() * validChoices.length);
   return validChoices[index];
 }
+
+const playerChoice = "paper";
+const computerChoice = getComputerChoice();
+
+function playRound(playerChoice, computerChoice) {
+  let playerValue = playerChoice.toUpperCase();
+  let computerValue = computerChoice.toUpperCase();
+  if (playerValue === computerValue) {
+    return `Draw! You selected ${playerChoice} and the computer selected ${computerChoice}`;
+  }
+  // Player wins
+  else if (
+    (playerValue === "SCISSORS" && computerValue === "PAPER") ||
+    (playerValue === "PAPER" && computerValue === "ROCK") ||
+    (playerValue === "ROCK" && computerValue === "SCISSORS")
+  ) {
+    return `You won! ${playerChoice} beats ${computerChoice}`;
+  } else {
+    return `You lost! ${computerChoice} beats ${playerChoice}`;
+  }
+}
+
+console.log(playRound(playerChoice, computerChoice));
