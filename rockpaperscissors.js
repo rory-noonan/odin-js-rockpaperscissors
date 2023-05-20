10219b7eff88335fa3c483f55c20d063809fbfe9
rory-noonan <92176688+rory-noonan@users.noreply.github.com>
2023-05-20 17:38:41 -0600

    Implement basic UI functionality

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,4 +1,12 @@
+const playButton = document.querySelector("#play");
+playButton.addEventListener("click", game);
+
+const resultsDisplay = document.querySelector("#results");
+const scoreDisplay = document.querySelector("#score");
+
 const validChoices = ["Rock", "Paper", "Scissors"];
+var playerScore = 0;
+var computerScore = 0;
 
 function getComputerChoice() {
   let index = Math.floor(Math.random() * validChoices.length);
@@ -9,40 +17,44 @@ function playRound(playerChoice, computerChoice) {
   let playerValue = playerChoice.toUpperCase();
   let computerValue = computerChoice.toUpperCase();
   if (playerValue === computerValue) {
-    console.log(
-      `Draw! You selected ${playerChoice} and the computer selected ${computerChoice}`
-    );
     return null;
   } else if (
     (playerValue === "SCISSORS" && computerValue === "PAPER") ||
     (playerValue === "PAPER" && computerValue === "ROCK") ||
     (playerValue === "ROCK" && computerValue === "SCISSORS")
   ) {
-    console.log(`You won! ${playerChoice} beats ${computerChoice}`);
     return true;
   } else {
-    console.log(`You lost! ${computerChoice} beats ${playerChoice}`);
     return false;
   }
 }
 
-function game() {
-  var playerScore = 0;
-  var computerScore = 0;
-  while (playerScore < 5 && computerScore < 5) {
-    let gameWon = playRound(prompt(), getComputerChoice());
-    switch (gameWon) {
-      case null:
-        break;
-      case true:
-        playerScore++;
-        break;
-      case false:
-        computerScore++;
-        break;
-    }
-    console.log(`Player: ${playerScore} Computer: ${computerScore}`);
-  }
+function refreshScoreDisplay(result) {
+  resultsDisplay.textContent = result;
+  scoreDisplay.textContent = `\nThe score is now Player ${playerScore} vs. Computer ${computerScore}`;
 }
 
-game();
+function getPlayerChoice() {
+  return "rock"; // Dummy data for now
+}
+
+function game() {
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  let gameWon = playRound(playerChoice, computerChoice);
+  let result = "";
+  switch (gameWon) {
+    case null:
+      result = `Draw! You selected ${playerChoice} and the computer selected ${computerChoice}`;
+      break;
+    case true:
+      playerScore++;
+      result = `You won! ${playerChoice} beats ${computerChoice}`;
+      break;
+    case false:
+      computerScore++;
+      result = `You lost! ${computerChoice} beats ${playerChoice}`;
+      break;
+  }
+  refreshScoreDisplay(result);
+}
